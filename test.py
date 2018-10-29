951c97b42f041905c29328bb5739d87c30ac3f2b
Osowo <4246265+osall2001@users.noreply.github.com>
2018-10-29 01:54:42 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,57 @@
+#!/bin/bash
+#
+#
+################################ Read Me ##########################################
+#
+#Better web scraping in Python with
+#Selenium (pip install Selenium)
+#Beautiful Soup (pip install BeautifulSoup4) - -bs4
+#pandas
+#
+# https://medium.freecodecamp.org/better-web-scraping-in-python-with-selenium-beautiful-soup-and-pandas-d6390592e251
+# https://www.pythonforbeginners.com/python-on-the-web/beautifulsoup-4-python/
+#
+#/Documents/Digital-Enterprize/webscraping$ python webtext-scrapingrev1.py
+#
+#Beautiful Soup is a Python library for pulling data out of HTML and XML files.
+#
+#################################################################################
+
+print ("""
+
+
+
+        /((((((\\\\ \n
+=======((((((((((\\\\\ \n
+     ((           \\\\\\\ \n
+     ( (*    _/      \\\\\\\ \n
+       \    /  \      \\\\\\________________ \n
+        |  |   |       </                  ((\\\\ \n
+        o_|   /        /                      \ \\\\    \\\\\\\ \n
+             |  ._    (                        \ \\\\\\\\\\\\\\\\ \n
+             | /                       /       /    \\\\\\\     \\ \n
+     .______/\/     /                 /       /         \\\ \n
+    / __.____/    _/         ________(       /\ \n
+   / / / ________/`---------'         \     /  \_ \n
+  / /  \ \                             \   \ \_  \ \n
+ ( <    \ \                             >  /    \ \ \n
+  \/     \\_                           / /       > ) \n
+          \_|                         / /       / / \n
+                                    _//       _// \n
+                                   /_|       /_| \n
+
+
+██╗    ██╗███████╗██████╗     ███████╗ ██████╗██████╗  █████╗ ██████╗ ███████╗██████╗
+██║    ██║██╔════╝██╔══██╗    ██╔════╝██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗
+██║ █╗ ██║█████╗  ██████╔╝    ███████╗██║     ██████╔╝███████║██████╔╝█████╗  ██████╔╝
+██║███╗██║██╔══╝  ██╔══██╗    ╚════██║██║     ██╔══██╗██╔══██║██╔═══╝ ██╔══╝  ██╔══██╗
+╚███╔███╔╝███████╗██████╔╝    ███████║╚██████╗██║  ██║██║  ██║██║     ███████╗██║  ██║
+ ╚══╝╚══╝ ╚══════╝╚═════╝     ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝
+
+ Knowledge Digestion Summarizer - Scraping List for Publishing and Graphics Creation
+
+""")
+
+#################################################################################
+
+print ('######################## Import Module Library ########################')
